dc338b096ba0ff0fd425b92f548621dc34537cc4
AnthonyBreyner <anthonyuzcategui7@gmail.com>
2015-06-11 14:14:48 -0430

    cambio

--- solicitarSoftware.php
+++ solicitarSoftware.php
@@ -33,7 +33,7 @@ if($resultado) {
     $mail->AltBody = "Texto Alternativo"; // optional, comment out and test
     $mail->MsgHTML ( $cuerpo );
     $address = $_POST['correo'];
-    $name = $_POST['nombres'] . " " . $_POST['apellidos'];
+    $name = $_POST['nombres'];
     $mail->AddAddress ( $address, $name );
 
     if (! $mail->Send ()) {
